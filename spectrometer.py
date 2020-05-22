65e5b4ab85e7fc1bb5bfa7a3fb78bdb36b86f854
Gabriel Cruz <gabrielcruz7400@gmail.com>
2020-05-22 10:48:50 -0300

    first commit

---
+++ spectrometer.py
@@ -0,0 +1,9 @@
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import *
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+
+import tkinter as tk
+from tkinter import *
