cb9d39d37c316dc959c49a83a37fb10de31c4cb9
Gabriel Cruz <gabrielcruz7400@gmail.com>
2020-05-25 11:16:44 -0300

    plotting a blank graph in the window

--- spectrometer.py
+++ spectrometer.py
@@ -1,12 +1,14 @@
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import *
+from matplotlib.backends.backend_tkagg import(
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib import style
+from matplotlib import pyplot as plt
 
 import tkinter as tk
-from tkinter import *
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 12)
 style.use("ggplot")
@@ -39,17 +41,38 @@ class Spectrometer(tk.Tk):
         frame= self.frames[cont]
         frame.tkraise()
 
+        
+
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Spectrometer", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
-          
 
-        #button1 = ttk.Button(self, text="Agree", command=lambda:  controller.show_frame(BTCe_page) )
-        #button1.pack()
+        f = Figure(figsize = (5,5), dpi = 100)
+        a = f.add_subplot(111)
+
+        x = []
+        y = []
+    
+        a.set_xlim(200, 850)
+        a.set_ylim(0, 40000)
+        
+        plt.plot(x, y)
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        button1 = ttk.Button(self, text="Acquire")
+        button1.pack()
 
 
 
 app= Spectrometer()
+#app.configure(background='grey')
 app.mainloop()
